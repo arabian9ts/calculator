d85622a482800312c90bd988129895043e2112a4
arabian9ts <nok14.tm59@gmail.com>
2017-06-18 23:59:03 +0900

    add: cells in storyboard

--- calculator/calculator/ViewController.swift
+++ calculator/calculator/ViewController.swift
@@ -8,17 +8,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let layout = UICollectionViewFlowLayout()
+        
+//        layout.itemSize = CGSize(wifth: 90, height: 90)
+        layout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0);
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    /** called when the cell is selected */
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+    }
+    
+    /** return number of cells */
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 19;
+    }
+    
+    /** set val in the cells */
+//    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+//        
+//    }
 
 
 }
