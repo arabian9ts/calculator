c4534b655263fe5c26071df502b51de5cd46e494
arabian9ts <nok14.tm59@gmail.com>
2017-06-22 23:45:08 +0900

    add: CustomCell files

---
+++ calculator/calculator/CustomCell.swift
@@ -0,0 +1,18 @@
+//
+//  CustomCell.swift
+//  calculator
+//
+//  Created by arabian9ts on 2017/06/22.
+//  Copyright © 2017年 arabian9ts. All rights reserved.
+//
+
+import UIKit
+
+class CustomCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
